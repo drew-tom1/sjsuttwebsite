0d382f92b001eaa4bcb48827b63b21445baa091f
Drew Tom <tom.drew261@gmail.com>
2025-07-12 14:43:38 -0500

    overscrolling fix for mobile

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,13 +21,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <Navbar />
       <body
         className={`${geistSans.variable} font-sans antialiased`}
       >
+        <Navbar />
         {children}
+        <Footer />
       </body>
-      <Footer />
     </html>
   );
 }
